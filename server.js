6210e1dff946af0d0812834d6c2843dba39275ae
Test3888 <lucashu@cmail.carleton.ca>
2024-10-20 22:26:13 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,66 @@
+/*
+"TOO SIMPLE HTTP SERVER"
+First attempt at an HTTP static server.
+It simply reads the contents of the requested file
+and sends that contents to the client.
+
+Here our node server does not try to analyse the url
+to "route" the requests, it simply serves
+whatever files happen to be in the ROOT_DIR directory.
+
+It's too simple because no attempt is made yet to correctly
+set the the content type for the client response.
+
+
+Use browser to view pages at
+   http://localhost:3000/index.html.
+*/
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+    let extname = path.extname(filePath);
+    let contentType = 'text/html';
+
+    // Set the content type based on the file extension
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+    }
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code == 'ENOENT') {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end('<h1>404 Not Found</h1>', 'utf-8');
+            } else {
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`, 'utf-8');
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
